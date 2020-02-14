f437f4f851e9cfe4f90dc6c2f762338b42778566
Aditya Pradita <pradita397@gmail.com>
2020-02-14 16:32:24 +0700

    Tidying up players list component

--- vue-football-club-finder/src/components/PlayerList.vue
+++ vue-football-club-finder/src/components/PlayerList.vue
@@ -1,109 +1,127 @@
 <template>
   <div class="hello">
 
-    <h3>Goalkeepers</h3>
-    
-    <ul class="row list-unstyled">
-        <li v-for="player in filteredPlayers('Goalkeeper')" :key="player.id" class="col-6 col-sm-6 col-md-3" >
-            <router-link class="card-link" :to="`/player-profile/${player.id}`" >
-              <div class="player-card">
-                  <header>
-                    <img src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png" class="img-fluid" :alt=player.name />
-                    <h4>49</h4>
-                  </header>
-                  <div class="player-body">
-                     <p>{{player.name}}</p>
-                  </div>
-              </div>
-            </router-link>
-        </li>
-    </ul>
+    <section class="players-section">
+      <h3>Goalkeepers</h3>
 
-    <h3>Defenders</h3>
-    
-    <ul class="row list-unstyled">
+      <ul class="row list-unstyled">
+        <li v-for="player in filteredPlayers('Goalkeeper')" :key="player.id" class="col-6 col-sm-6 col-md-3">
+          <router-link class="card-link" :to="`/player-profile/${player.id}`">
+            <div class="player-card">
+              <header>
+                <img
+                  src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png"
+                  class="img-fluid" :alt=player.name />
+                <h4>49</h4>
+              </header>
+              <div class="player-body">
+                <p>{{player.name}}</p>
+              </div>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+    </section>
+
+    <section class="players-section">
+      <h3>Defenders</h3>
+
+      <ul class="row list-unstyled">
         <li v-for="player in filteredPlayers('Defender')" :key="player.id" class="col-6 col-sm-6 col-md-3">
-            <router-link class="card-link" :to="`/player-profile/${player.id}`" >
-              <div class="player-card">
-                  <header>
-                    <img src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png" class="img-fluid" :alt=player.name />
-                    <h4>49</h4>
-                  </header>
-                  <div class="player-body">
-                     <p>{{player.name}}</p>
-                  </div>
+          <router-link class="card-link" :to="`/player-profile/${player.id}`">
+            <div class="player-card">
+              <header>
+                <img
+                  src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png"
+                  class="img-fluid" :alt=player.name />
+                <h4>49</h4>
+              </header>
+              <div class="player-body">
+                <p>{{player.name}}</p>
               </div>
-            </router-link>
+            </div>
+          </router-link>
         </li>
-    </ul>
+      </ul>
+    </section>
 
-    <h3>Midfielders</h3>
-    
-    <ul class="row list-unstyled">
+    <section class="players-section">
+      <h3>Midfielders</h3>
+
+      <ul class="row list-unstyled">
         <li v-for="player in filteredPlayers('Midfielder')" :key="player.id" class="col-6 col-sm-6 col-md-3">
-            <router-link class="card-link" :to="`/player-profile/${player.id}`" >
-                <div class="player-card">
-                  <header>
-                    <img src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png" class="img-fluid" :alt=player.name />
-                    <h4>49</h4>
-                  </header>
-                  <div class="player-body">
-                     <p>{{player.name}}</p>
-                </div>
+          <router-link class="card-link" :to="`/player-profile/${player.id}`">
+            <div class="player-card">
+              <header>
+                <img
+                  src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png"
+                  class="img-fluid" :alt=player.name />
+                <h4>49</h4>
+              </header>
+              <div class="player-body">
+                <p>{{player.name}}</p>
               </div>
-            </router-link>
+            </div>
+          </router-link>
         </li>
-    </ul>
+      </ul>
+    </section>
 
-    <h3>Attackers</h3>
-    
-    <ul class="row list-unstyled">
+    <section class="players-section">
+      <h3>Attackers</h3>
+
+      <ul class="row list-unstyled">
         <li v-for="player in filteredPlayers('Attacker')" :key="player.id" class="col-6 col-sm-6 col-md-3">
-            <router-link class="card-link" :to="`/player-profile/${player.id}`" >
-                <div class="player-card">
-                    <header>
-                      <img src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png" class="img-fluid" :alt=player.name />
-                      <h4>49</h4>
-                    </header>
-                    <div class="player-body">
-                      <p>{{player.name}}</p>
-                    </div>
-                </div>
-            </router-link>
+          <router-link class="card-link" :to="`/player-profile/${player.id}`">
+            <div class="player-card">
+              <header>
+                <img
+                  src="http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/Photo-Missing.png"
+                  class="img-fluid" :alt=player.name />
+                <h4>49</h4>
+              </header>
+              <div class="player-body">
+                <p>{{player.name}}</p>
+              </div>
+            </div>
+          </router-link>
         </li>
-    </ul>
-      
+      </ul>
+    </section>
+
+
+
+
+
+
+
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'PlayerList',
-  props: ['players'],
-  methods: {
-    filteredPlayers(position) {
-      return this.players.filter(player => {
-        return player.position == position      
-      })
+  export default {
+    name: 'PlayerList',
+    props: ['players'],
+    methods: {
+      filteredPlayers(position) {
+        return this.players.filter(player => {
+          return player.position == position
+        })
+      },
+      filteredPlayersDebug(position) {
+        let filteredPlayer = this.players.filter(player => {
+          return player.position == position
+        })
+        return filteredPlayer;
+      },
     },
-    filteredPlayersDebug(position) {
-      let filteredPlayer = this.players.filter(player => {
-        return player.position == position      
-      })
-      return filteredPlayer;
-    },
-  },
-  created() {
-    console.log(this.filteredPlayers("Defenders"))
+    created() {
+      console.log(this.filteredPlayers("Defenders"))
+    }
   }
-}
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 
 </style>
-
-
