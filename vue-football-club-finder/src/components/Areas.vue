5df0d2041503108e0a6cabce6baaad33a97340ed
Aditya Pradita <pradita397@gmail.com>
2020-02-14 15:25:09 +0700

    Styling area component

--- vue-football-club-finder/src/components/Areas.vue
+++ vue-football-club-finder/src/components/Areas.vue
@@ -1,97 +1,61 @@
-
 <template>
   <div class="hello">
-      <ul class="row list-unstyled">
-        <!-- if parent Area = World -->
-        <li v-for="area in areas" :key="area.id" class="col-6 col-sm-6 col-md-3">
-            <router-link :to="`/${area.id}/country-list`"  v-if="area.ensignUrl">
-                <img :src=area.ensignUrl width="100%" :alt=area.name class="img-fluid" />
-                <p>{{area.name}}</p>
+    <ul class="row list-unstyled">
+      <!-- if parent Area = World -->
+      <li v-for="area in areas" :key="area.id" class="col-6 col-sm-6 col-md-3">
+        <router-link :to="`/${area.id}/country-list`">
+          <div class="area-card">
+            <p>{{area.name}}</p>
+          </div>
+        </router-link>
+      </li>
+    </ul>
 
-            </router-link>
-            <router-link :to="`/${area.id}/country-list`" v-else>
-                <div class="blank-flag">
-                    <p>{{area.name}} Flag</p>
-                </div>
-                <p>{{area.name}}</p>
-            </router-link>
-        </li>
-        </ul>
-      
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+  import axios from 'axios';
 
-export default {
-  name: 'Areas',
-  data() {
-    return {
-      areas: [],
-      errors: []
-    }
-  },
-
-  // Fetches posts when the component is created.
-  created() {
-    let config = {
-        headers: {
-            "X-Auth-Token": this.api_key,
-        }
-    }
-
-    axios.get(`https://api.football-data.org/v2/areas/`, config)
-    .then(response => {
-      // JSON responses are automatically parsed.
-      if(this.$route.params.id) {
-        this.areas = response.data.areas.filter(area => area.parentAreaId === this.$route.params.id)
-      } else {
-          this.areas = response.data.areas.filter(area => area.parentAreaId === 2267);
+  export default {
+    name: 'Areas',
+    data() {
+      return {
+        areas: [],
+        errors: []
       }
-      
-    })
-    .catch(e => {
-      this.areas.push(e)
-    })
+    },
 
-    console.log(this.areas);
+    // Fetches posts when the component is created.
+    created() {
+      let config = {
+        headers: {
+          "X-Auth-Token": this.api_key,
+        }
+      }
+
+      axios.get(`https://api.football-data.org/v2/areas/`, config)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          if (this.$route.params.id) {
+            this.areas = response.data.areas.filter(area => area.parentAreaId === this.$route.params.id)
+          } else {
+            this.areas = response.data.areas.filter(area => area.parentAreaId === 2267);
+          }
+
+        })
+        .catch(e => {
+          this.areas.push(e)
+        })
+
+      console.log(this.areas);
 
 
+    }
   }
-}
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-}
-a {
-  color: #42b983;
-}
-.blank-flag{
-    height: 200px;
-    width: 100%;
-    background-color: gray;
-    font-size: 24px;
-    text-align: center;
-    p{
-        line-height: 200px;
-        color: white;
-    }
-    @media only screen and (max-width: 480px) {
-        height: 78px;
-    }
-}
+
 </style>
-
-
